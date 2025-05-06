67d3db6bb00452811db096e4ff2c7a513b61d02a
Mohamedselloum <mohsell2003@gmail.com>
2025-05-06 09:00:59 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true // Exposes the dev server on your network
+    host: true
   }
 });
